bb2a07d449cf211e2aeabcf192529b5c6b6f688d
katmardo <katmardo@gmail.com>
2026-04-19 12:44:20 -0500

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/indie-books-mn/",
-});
+  base: mode === "production" ? "/" : "/indie-books-mn/",
+}));
