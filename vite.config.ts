ce6752c8e487dbefac6e7a8dff3d24b65f3ff475
katmardo <katmardo@gmail.com>
2026-04-18 19:42:09 -0500

    edit build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-//comment
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/indie-books-mn/",
